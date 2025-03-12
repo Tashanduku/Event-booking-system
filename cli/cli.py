6a8e77cd43c81f7c53e1699ff682924014234665
Natasha Nduku <natashanduku0@gmail.com>
2025-03-12 12:26:42 +0300

    imports database and creates tables

--- cli/cli.py
+++ cli/cli.py
@@ -1,4 +1,10 @@
 import click
+from models.db import SessionLocal, engine
+from models.event import Event, Booking
+
+from models.db import Base
+Base.metadata.create_all(bind=engine)
+
 
 @click.group()
 def cli():
