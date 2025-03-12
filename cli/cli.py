c56713111dcb35dc98d097098832c7112ac29fce
Natasha Nduku <natashanduku0@gmail.com>
2025-03-12 12:32:31 +0300

    creates the book event command

--- cli/cli.py
+++ cli/cli.py
@@ -11,6 +11,8 @@ def cli():
     """Event Booking CLI"""
     pass
 
+
+# add event command
 @click.command()
 @click.option("--name", prompt="Event Name", help="Name of the event")
 @click.option("--location", prompt="Location", help="Location of the event")
@@ -36,4 +38,28 @@ def list_events():
     else:
         for event in events:
             click.echo(f"{event.id}: {event.name} - {event.location} on {event.date} ({event.available_tickets} tickets left)")
+    session.close()
+
+#book event 
+@click.command()
+@click.option("--event_id", prompt="Event ID", type=int, help="ID of the event to book")
+@click.option("--user", prompt="Your Name", help="Your name for the booking")
+def book_ticket(event_id, user):
+    """Book a ticket for an event"""
+    session = SessionLocal()
+    event = session.query(Event).filter_by(id=event_id).first()
+    
+    if not event:
+        click.echo("Event not found.")
+        return
+    
+    if event.available_tickets > 0:
+        booking = Booking(user_name=user, event_id=event.id)
+        event.available_tickets -= 1
+        session.add(booking)
+        session.commit()
+        click.echo(f"Ticket booked for {user} at '{event.name}'")
+    else:
+        click.echo("No tickets available.")
+    
     session.close()
